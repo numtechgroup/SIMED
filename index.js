51b780ad39c69afb8201a92c0cd30698013f0928
Papa Ibrahima SOW <papaibrahimasow@esp.sn>
2024-01-12 12:12:56 +0000

    ordonnance downloading

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors({
   methods: 'POST, GET, PUT, OPTIONS, DELETE',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
 }));
-
+//test
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://simed-six.vercel.app");
   // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
